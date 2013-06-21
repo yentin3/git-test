19ebde0fb9e25728e238d037ed953609db9f80b7
yentin3 <yentin3@gmail.com>
2013-06-21 14:51:16 -0700

    more changes to Min_Max

--- src/Min_Max.java
+++ src/Min_Max.java
@@ -14,4 +14,13 @@ public class Min_Max {
 				min = A[i];
 		return min;
 	}
+	
+	public static int Max(int[] A, int n)
+	{
+		int max = A[0];
+		for(int i = 1; i < n; i++)
+			if(max > A[i])
+				max = A[i];
+		return max;
+	}
 }
