1c7ed5dbd0550bfbbfc0cb8a97bd77b7660f5351
yentin3 <yentin3@gmail.com>
2014-07-08 11:10:56 -0700

    add something inside files

--- src/Min_Max.java
+++ src/Min_Max.java
@@ -4,23 +4,28 @@ public class Min_Max {
 	public static void main(String[] args)
 	{
 		int[] A= {1, 4, -3, 4};
-		 Min(A, 4);
+        System.out.println(Min(A, 4));
+        System.out.println("Hello GitHub!");
 	}
 	public static int  Min(int[] A, int n)
 	{
 		int min = A[0];
-		for(int i = 1; i < n; i++)
-			if(min > A[i])
-				min = A[i];
+		for(int i = 1; i < n; i++) {
+            if(min > A[i]) {
+                min = A[i];
+            }
+        }
 		return min;
 	}
 	
 	public static int Max(int[] A, int n)
 	{
 		int max = A[0];
-		for(int i = 1; i < n; i++)
-			if(max <A[i])
-				max = A[i];
+		for(int i = 1; i < n; i++) {
+            if(max <A[i]) {
+                max = A[i];
+            }
+        }
 		return max;
 	}
 }
